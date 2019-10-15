701dbf0e21b242b4f75763e54ea78ad6976c282a
c1250082710 <1250082710@qq.com>
2019-10-15 15:08:28 +0800

    work5

---
+++ work5/work5.js
@@ -0,0 +1,35 @@
+//9x9空表格
+Str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=9;j>0;--j){
+        str +='<td>&nbsp;</td>'
+    }
+    str += '</tr>'
+}
+str +='</table>'
+document.getElementById('table1').innerHTML = str
+
+
+str = '<table border="1">'
+for(let i=9;i>0;--j){
+    str +='<tr>'
+    for(var j=i;j>0;--j){
+        str +='<tr>&nbsp;</tr>'
+    }
+    str += '</tr>'
+}
+str +='</table>'
+document.getElementById('table2').innerHTML = str
+
+
+str = '<table border="1">'
+for(let i=9;i>0;--j){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str +='<td>&nbsp;'+1+'x'+j+'='+(i*j) +'</td>'
+    }
+    str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table3').innerHTML = str
